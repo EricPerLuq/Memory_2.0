55bb56a059e4e1caf52c360827fd2f816348aa9c
pvalls4 <valls.vilalta.4@gmail.com>
2021-10-14 19:40:54 +0200

    Spec 6 y 8

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
-girarCarta(idcarta, anverso) {
+girarCarta(idcarta, anverso, contGiradas) {
 	carta=document.getElementById(idcarta).src;
 	console.log(carta[-11,-1]==="reverso.png");
-	if (!carta[-11,-1]==="reverso.png") {
+	if (!carta[-11,-1]==="reverso.png" && contGiradas<2) {
 		carta = "./imgs/" + anverso + ".png";
 	} 
 }
