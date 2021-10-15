e86c54930dfd7389fd3ec62f742c53efcdced413
pvalls4 <valls.vilalta.4@gmail.com>
2021-10-15 15:46:30 +0200

    Listo para integración spec 6

--- functions.js
+++ functions.js
@@ -1,7 +1,35 @@
+var contGiradas=0;
+var idPrimeraCarta="";
+var primeraCarta="";
+var cartaActual="";
+
 girarCarta(idcarta, anverso, contGiradas) {
-	carta=document.getElementById(idcarta).src;
-	console.log(carta[-11,-1]==="reverso.png");
-	if (!carta[-11,-1]==="reverso.png" && contGiradas<2) {
-		carta = "./imgs/" + anverso + ".png";
-	} 
+	if (contGiradas==0) {
+		idPrimeraCarta=idcarta;
+	} else {
+		primeraCarta=document.getElementById(idPrimeraCarta).src
+	}
+
+	cartaActual=document.getElementById(idcarta).src;
+	console.log(cartaActual);
+	console.log(cartaActual[-11,-1]==="reverso.png");
+	if (!cartaActual[-11,-1]==="reverso.png" && contGiradas<2) {
+		cartaActual = "./imgs/" + anverso + ".png";
+		contGiradas+=1;
+		if (contGiradas==1) {
+			primeraCarta=cartaActual;
+		} else {
+			comprarCartas(primeraCarta);
+		}
+	}
+}
+
+comprarCartas(primeraCarta){
+	if (primeraCarta!=cartaActual) {
+		primeraCarta="./imgs/revers.png";
+		cartaActual="./imgs/revers.png";
+	} else {
+		//contadorGanador = document.getElementById("contador"); //posiblemente le falte un metodo para el contenido del p
+		//document.getElementById("contador").innerText = contadorGanador+1; //dudosa eficacia
+	}
 }
