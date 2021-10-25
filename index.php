df4a085f8fba3bfd07eb81ab21d9fa0da5d97a20
Montserrat Paus <monserratpaus@gmail.com>
2021-10-25 19:10:16 +0200

    spec26

--- index.php
+++ index.php
@@ -7,28 +7,32 @@
 	<title>Memory 2.0</title>
 </head>
 <body>
-	<table align="center">
-		<tr>
-			<td rowspan="2">
-				<h1 align="center"><span class="casilla">MEMORY 2.0</span></h1>
-				<h2><span class="casilla">INSTRUCCIONES</span></h2>
-				<p>· Debes girar 2 cartas por cada turno.</p>
-				<p>· Si las cartas son las mismas, sumas un punto y se quedan desbloqueadas.</p>
-				<p>· Si las cartas no coinciden, tienes 2 segundos para memorizarlas antes de que se vuelvan a ocultar.</p>
-				<p>· La partida acaba cuando has encontrado todas las parejas.</p>
-				<p>· El tiempo y el número de intentos son ilimitados.</p>
-			</td>
-			<td rowspan="2">
-				<img class=separacion src="./imgs/whiteline.png">
-			</td>
-			<td>
-				<img src="./imgs/CartaPortada.png">
-			</td>
-		</tr>
+	<div align="center">
+        <img src="/imgs/titulo.png">
+    </div>
+    <table align="center">
+        <tr>
+            <td rowspan="3">
+                <h1><span class="casilla">INSTRUCCIONES</span></h1>
+                <p>· Debes girar 2 cartas por cada turno.</p>
+                <p>· Si las cartas son las mismas, sumas un punto y se quedan desbloqueadas.</p>
+                <p>· Si las cartas no coinciden, tienes 2 segundos para memorizarlas antes de que se vuelvan a ocultar.</p>
+                <p>· La partida acaba cuando has encontrado todas las parejas.</p>
+                <p>· El tiempo y el número de intentos son ilimitados.</p>
+            </td>
+            <td rowspan="2">
+                <img class="separacion" src="./imgs/whiteline.png">
+            </td>
+            <td>
+                <img class="imagen" src="./imgs/CartaPortada.png">
+            </td>
+        </tr>
+
 		<tr>
 			<td>
 				<form action="Juego.php">
-			        <input class="casilla" type="submit" value="JUGAR"  onclick="Jugar()"/>
+					<input type="text" name="nombreUsuario" placeholder="Nombre" required/>
+                    <input class="casilla" type="submit" value="JUGAR"  onclick="Jugar()"/>
 				</form>
 			</td>
 		</tr>
